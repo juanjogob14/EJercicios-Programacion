ca4a6b4f6199d43b67659a38f4cd7610259a6da4
Juanjo <juanjosegoberna.figueira@gmail.com>
2021-02-28 22:44:09 +0100

    Ejer5 Tema5 DI

---
+++ Ejer5/Ejer5/ValidateTextBox.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejer5
+{
+    public partial class ValidateTextBox : UserControl
+    {
+        private bool correcto;
+        Color color = Color.Red;
+
+        public bool Correcto
+        {
+            set
+            {
+                correcto = value;
+                Refresh();
+            }
+            get
+            {
+                return correcto;
+            }
+        }
+
+        private string texto;
+
+        public string Texto
+        {
+            set
+            {
+                textBox1.Text = value;
+                Refresh();
+            } 
+            get
+            {
+                return textBox1.Text;
+            }
+        } 
+
+        private Boolean multilinea;
+
+        public Boolean Multilinea
+        {
+            set
+            {
+                textBox1.Multiline = value ;
+                Refresh();
+            } 
+            get
+            {
+                return textBox1.Multiline;
+            }
+        }
+        public enum eTipo
+        {
+            NUMERICO, TEXTUAL
+        }
+
+        private eTipo tipo = eTipo.TEXTUAL;
+
+        public eTipo Tipo
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(eTipo),value))
+                {
+                    tipo = value;
+                    //cambiaTipo();
+                    Refresh();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return tipo;
+            }
+        }
+
+        private void cambiaTipo()
+        {
+            Correcto = true;
+            switch (tipo)
+            {
+                case eTipo.NUMERICO:
+                    for (int i = 0; i < Texto.Length; i++)
+                    {
+                        if (Texto[i] >= 48 && Texto[i] <= 57 || Texto[i]==10 || Texto[i] == 13)
+                        {
+
+                            if (Correcto)
+                            {
+                                color = Color.Green;
+                                Refresh();
+                            }
+                            
+                        }
+                        else
+                        {
+                            Correcto = false;
+                            color = Color.Red;
+                            Refresh();
+                        }
+                    }
+                    break;
+                case eTipo.TEXTUAL:
+                    for (int i = 0; i < Texto.Length; i++)
+                    {
+                        if (Texto[i] >= 65 && Texto[i]<=90 || Texto[i]>=97 && Texto[i]<=122 || Texto[i] == 32 || Texto[i] == 10 || Texto[i] == 13)
+                        {
+
+                            if (Correcto)
+                            {
+                                color = Color.Green;
+                                Refresh();
+                            }
+                        }
+                        else
+                        {
+                            Correcto = false;
+                            color = Color.Red;
+                            Refresh();
+                        }
+                    }
+                    break;
+                    
+            }
+        }
+
+        public ValidateTextBox()
+        {
+            InitializeComponent();
+            
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            Graphics g = e.Graphics;
+            this.Height = textBox1.Height + 20;
+            textBox1.Width = this.Width - 20;
+            
+            SolidBrush b = new SolidBrush(color);
+            g.FillRectangle(b, 5, 5, this.Width - 5, this.Height - 5);
+               
+            
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            cambiaTipo();
+        }
+    }
+}
---
+++ Ejer5/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.validateTextBox1 = new Ejer5.ValidateTextBox();
+            this.SuspendLayout();
+            // 
+            // validateTextBox1
+            // 
+            this.validateTextBox1.AutoSize = true;
+            this.validateTextBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.validateTextBox1.Correcto = false;
+            this.validateTextBox1.Location = new System.Drawing.Point(127, 113);
+            this.validateTextBox1.Multilinea = true;
+            this.validateTextBox1.Name = "validateTextBox1";
+            this.validateTextBox1.Size = new System.Drawing.Size(494, 42);
+            this.validateTextBox1.TabIndex = 0;
+            this.validateTextBox1.Texto = "";
+            this.validateTextBox1.Tipo = Ejer5.ValidateTextBox.eTipo.TEXTUAL;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.validateTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Ejer5.ValidateTextBox validateTextBox1;
+    }
+}
+
